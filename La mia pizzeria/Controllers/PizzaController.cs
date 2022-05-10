9d30b000c800c735c312fcf62fef1c98c3082a1f
Alec <alemor98@hotmail.it>
2022-05-10 12:27:50 +0200

    Add project files.

---
+++ La mia pizzeria/Controllers/PizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace La_mia_pizzeria.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
