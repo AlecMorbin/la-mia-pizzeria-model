31f9a840011b1f8ea6bb3cc9df055817995044bb
Alec <alemor98@hotmail.it>
2022-05-10 13:07:30 +0200

    Creata classe Utility e classe modello

---
+++ La mia pizzeria/Utils/PizzaData.cs
@@ -0,0 +1,23 @@
+﻿using La_mia_pizzeria.Models;
+
+namespace La_mia_pizzeria.Utils
+{
+    public class PizzaData
+    {
+        private static List<PizzaData> pizzas = new List<PizzaData>();
+
+        public static List<PizzaData> GetPizzas()
+        {
+            if( pizzas != null )
+                return pizzas;
+
+            for (int i = 0; i < 0; i++)
+            {
+                Pizza pizza = new Pizza(i,"Pizza n-" +i, "Lorem Ipsum is simply dummy text of the printing and typesetting... ","/img/pizza-margherita");
+            }
+
+
+            return pizzas;
+        }
+    }
+}
